c5d78630dba79501339bb7aa1623e7db74805429
Nicol Ortiz <nicolvaleria0919@gmail.com>
2026-09-20 15:35:45 -0500

    refactor(resetPassword): update success response state

--- controllers/auth.js
+++ controllers/auth.js
@@ -167,6 +167,6 @@ export class AuthController {
       return res.status(401).json({ error: 'Invalid or expired token. Do the process again.' })
     }
 
-    res.json({ message: 'Password updated successfully' })
+    res.status(200).json({ message: 'Password updated successfully' })
   }
 }
